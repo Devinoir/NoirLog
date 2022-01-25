f5714715503c8deb17cd023345e69ff9b3b96b66
Devinoir <62542694+Devinoir@users.noreply.github.com>
2022-01-25 18:22:43 +0100

    Projektdateien hinzufügen.

---
+++ NoirLog/NoirLog.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace NoirLog
+{
+    public class NoirLogger
+    {
+        public string Path { get; set ; }
+
+        public enum LogLevel
+        {
+            Info = 0,
+            Debug = 1,
+            Warning = 2,
+            Error = 3
+        }
+
+        public NoirLogger(string path)
+        {
+            Path = path;
+        }
+
+        public void WriteToFile(LogLevel logLevel, string text)
+        {
+            string timeStamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            string logText = $"{timeStamp} - [{logLevel.ToString().ToUpper()}]: {text}\n";
+            File.AppendAllText(Path, logText);
+        }
+
+        public void Info(string text)
+        {
+            string timeStamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            string logText = $"{timeStamp} - [INFO]: {text}\n";
+            File.AppendAllText(Path, logText);
+        }
+
+        public void Debug(string text)
+        {
+            string timeStamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            string logText = $"{timeStamp} - [DEBUG]: {text}\n";
+            File.AppendAllText(Path, logText);
+        }
+
+        public void Warning(string text)
+        {
+            string timeStamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            string logText = $"{timeStamp} - [WARNING]: {text}\n";
+            File.AppendAllText(Path, logText);
+        }
+
+        public void Error(string text)
+        {
+            string timeStamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            string logText = $"{timeStamp} - [ERROR]: {text}\n";
+            File.AppendAllText(Path, logText);
+        }
+    }
+}
---
+++ NoirLogTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NoirLog;
+
+namespace NoirLogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new NoirLogger(@"C:\Users\devin\Repository\log.txt");
+
+            logger.Info("Info Test");
+            logger.Debug("Debug Test");
+            logger.Warning("Warning Test");
+            logger.Error("Error Test");
+
+            Console.Read();
+        }
+    }
+}
